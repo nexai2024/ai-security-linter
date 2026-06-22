2a67f83c5a04b6a11d8e2bdb06e5338d331793d9
David Parker <nexai.coach@gmail.com>
2026-06-22 18:09:58 -0400

    Add updateUser function to update user data

---
+++ actions2.ts
@@ -0,0 +1,7 @@
+'use server'
+import { prisma } from './db' // dummy import
+import * as React from 'react';
+export async function updateUser(data: any) {
+  // No auth check!
+  await prisma.user.update({ where: { id: data.id }, data })
+}
