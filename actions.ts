15dafcd7b3b3b825f57fb2cfe73087d1096645e6
David Parker <nexai.coach@gmail.com>
2026-06-22 18:04:14 -0400

    Remove unused React import from actions.ts

--- actions.ts
+++ actions.ts
@@ -1,6 +1,6 @@
 'use server'
 import { prisma } from './db' // dummy import
-import * as React from 'react';
+
 export async function updateUser(data: any) {
   // No auth check!
   await prisma.user.update({ where: { id: data.id }, data })
